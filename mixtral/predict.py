a28992fad772062eb37318ae10f27064b83877a3
Nathan Raw <nxr9266@g.rit.edu>
2023-12-12 05:15:52 +0000

    :truck: Use full ckpt not shards

--- mixtral/predict.py
+++ mixtral/predict.py
@@ -10,15 +10,7 @@ from src import Llama
 MODEL_ID = "mixtral-8x7b-32kseqlen"
 WEIGHTS_URL = "https://weights.replicate.delivery/hf/mixtral-8x7b-32kseqlen"
 REMOTE_FILES = [
-    "consolidated.00.pth-split00.pth",
-    "consolidated.00.pth-split01.pth",
-    "consolidated.00.pth-split02.pth",
-    "consolidated.00.pth-split03.pth",
-    "consolidated.00.pth-split04.pth",
-    "consolidated.00.pth-split05.pth",
-    "consolidated.00.pth-split06.pth",
-    "consolidated.00.pth-split07.pth",
-    "consolidated.00.pth-split08.pth",
+    "consolidated.00.pth",
     "params.json",
     "tokenizer.model",
 ]
@@ -27,33 +19,19 @@ DEFAULT_TEMPERATURE = 0.6
 DEFAULT_TOP_P = 0.9
 
 
-def consolidate_shards(prefix, output_file, num_shards, drop_shards=False):
-    if os.path.exists(output_file):
-        print("Output file already exists, skipping consolidation")
-        return
-    print(f"Consolidating {num_shards} shards into {output_file}")
-    shard_files = [f"{prefix}-split{i:02d}.pth" for i in range(num_shards)]
-    with open(output_file, 'wb') as outfile:
-        subprocess.run(['cat'] + shard_files, stdout=outfile)
-    if drop_shards:
-        for shard in shard_files:
-            os.remove(shard)
-
 def load_weights():
-    if not os.path.exists("weights-cache/consolidated.00.pth"):
-        start = time.time()
-        maybe_download_with_pget(
-            path="weights-cache",
-            remote_path=WEIGHTS_URL,
-            remote_filenames=REMOTE_FILES,
-        )
-        print(f"downloading weights took {time.time() - start:.3f}s...now consolidating shards...")
-        consolidate_shards('weights-cache/consolidated.00.pth', 'weights-cache/consolidated.00.pth', 9, drop_shards=False)
+    start = time.time()
+    maybe_download_with_pget(
+        path="weights-cache",
+        remote_path=WEIGHTS_URL,
+        remote_filenames=REMOTE_FILES,
+    )
+    print(f"downloading weights took {time.time() - start:.3f}s...")
+
 
 class Predictor(BasePredictor):
     def setup(self):
         load_weights()
-        # consolidate_shards('weights-cache/consolidated.00.pth', 'weights-cache/consolidated.00.pth', 9)
         self.model = Llama.build(
             ckpt_dir="weights-cache",
             tokenizer_path="weights-cache/tokenizer.model",
