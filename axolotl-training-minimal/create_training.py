d155ebb1d9e4418664908f2753840b0d45b8470f
Nathan Raw <nxr9266@g.rit.edu>
2024-01-15 05:20:43 +0000

    :sparkles: Add missing configs, create_training.py

---
+++ axolotl-training-minimal/create_training.py
@@ -0,0 +1,23 @@
+import replicate
+from replicate.exceptions import ReplicateException
+
+training_model_id = "nateraw/axolotl-trainer:2805a0dd68b361a3a77fcbbcca4590ef5165b4c668ce790571c3ff820e360fc4"
+dest_model_id = "nateraw/test-tinyllama-english-to-hinglish"
+visibility = "private"
+
+try:
+    replicate.models.create(
+        owner=dest_model_id.split("/")[0],
+        name=dest_model_id.split("/")[1],
+        visibility=visibility,
+        hardware="gpu-a40-large"  # This is the hardware used for inference
+    )
+except ReplicateException as e:
+    print("Model already exists")
+
+training = replicate.trainings.create(
+  version=training_model_id,
+  input={"config": open("config/tinyllama_debug.yaml", "rb")},
+  destination=dest_model_id,
+)
+print(training)
